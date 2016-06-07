20e4198eb7278c911c318bd42dbb98e55cc8f254
boochoo <fromaddistohelsinki@gmail.com>
2016-06-07 21:03:45 +0300

    Login API

--- app/routes/api.js
+++ app/routes/api.js
@@ -3,7 +3,19 @@
 var User = require('../models/user');
 var config = require('../../config');
 var secretKey = config.secretKey;
+var jsonwebtoken = require('jsonwebtoken');
 
+function createToken(user){
+    var token = jsonwebtoken.sign({
+        _id : user._id,
+        name : user.name,
+        username :user.username
+    }, secretKey, {
+        expiresIn: 1440
+    });
+
+    return token;
+}
 module.exports = function(app, express){
 
     var api = express.Router();
@@ -12,6 +24,7 @@ module.exports = function(app, express){
 
         var user = new User({
             //body refers to bodyParser
+
             name : req.body.name,
             username : req.body.username,
             password : req.body.password
@@ -25,6 +38,51 @@ module.exports = function(app, express){
 
             res.json({ message : 'User has been created!'})
         });
+    });
+
+    api.get('/users', function(req, res){
+
+        User.find({}, function(err, users){
+            if(err){
+                res.send(err);
+                return;
+            }
+            res.json(users);
+        });
+    });
+
+    api.post('/login', function(req, res){
+
+        User.findOne({
+            username : req.body.username
+        }).select('password').exec(function(err, user){
+
+            if(err){
+                throw err;
+            }
+            if(!user){
+                res.send({message : 'User doesnt exist!'});
+            }else if(user){
+                var validPassword = user.comparePassword(req.body.password);
+
+                if(!validPassword){
+                    res.send({message : "Invalid Password!"})
+                }else{
+
+                    ////token
+
+                    var token = createToken(user);
+
+                    res.send({
+                        success : true,
+                        message : 'Successfully logged in!',
+                        token : token
+                    })
+                }
+            }
+
+        });
+
     });
 
     return api

bea33d0ac1ef4166dc6dd56332faf874c08dc6b6
boochoo <fromaddistohelsinki@gmail.com>
2016-06-07 17:00:21 +0300

    Compare passwords with a custom method

--- app/model/user.js
+++ app/model/user.js
@@ -5,6 +5,7 @@
 var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 
 var UserSchema = new Schema({
 
@@ -21,8 +22,34 @@ var UserSchema = new Schema({
         required : true,
         select : false
     }
+});
 
+UserSchema.pre('save', function (next) {
+    //this obviously points to UserSchema
+    var user = this;
+
+    //if the user password is not modified
+
+    if(!user.isModified('password'))
+        return next();
+
+    bcrypt.hash(user.password, null, null, function(err, hash){
+        if(err){
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    });
 
 });
+
+//Custom method for password comparison
+
+UserSchema.methods.comparePassword = function(password){
+    var user = this;
+
+    //compare the new and the former passwords
+    return bcrypt.compareSync(password, user.password);
+}
 
 module.exports = mongoose.model('User', UserSchema);
